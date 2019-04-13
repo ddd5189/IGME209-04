b74b3168d949c60de1e9f77ce8b71f913b2213f5
Drew Donovan <ddd5189@g.rit.edu>
2019-04-12 20:09:42 -0400

    Added beginning code for Rule of Three

--- HW5/HW5/Stack.h
+++ HW5/HW5/Stack.h
@@ -1,20 +1,42 @@
 #pragma once
+#include <iostream>
+using namespace std;
 template <class T>
 class Stack
 {
 public:
 	Stack();
+	//copy constructor
+	Stack(const Stack& other);
+	//copy aassignment operator
+	Stack& operator= (const Stack& other); 
 	void Push(T);
 	void Pop(T);
 	void Print(T);
 	~Stack();
 };
 
+
 template <class T>
 Stack<T>::Stack()
 {
 
 }
+
+//copy constructor
+template <class T>
+Stack<T>::Stack(const Stack& other) : data()
+{
+	
+}
+
+// copy assignment operator
+//template <class T>
+//Stack<T>::Stack& operator= (const Stack& other)
+//{
+//	//if(this != &other)
+//	
+//}
 
 template <class T>
 void Stack<T>::Push(T var)
